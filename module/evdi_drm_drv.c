9dd570796c08dc63aff5a17087c4f2f4c2fc076e
Alex Biddulph <abiddulph@4tel.com.au>
2023-11-10 10:18:37 +1100

    Fix code style

--- module/evdi_drm_drv.c
+++ module/evdi_drm_drv.c
@@ -124,11 +124,11 @@ static struct drm_driver driver = {
 
 	.fops = &evdi_driver_fops,
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 6, 0)
+#if KERNEL_VERSION(6, 6, 0) > LINUX_VERSION_CODE
 	.prime_fd_to_handle = drm_gem_prime_fd_to_handle,
 #endif
 	.gem_prime_import = drm_gem_prime_import,
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 6, 0)
+#if KERNEL_VERSION(6, 6, 0) > LINUX_VERSION_CODE
 	.prime_handle_to_fd = drm_gem_prime_handle_to_fd,
 #endif
 #if KERNEL_VERSION(5, 11, 0) <= LINUX_VERSION_CODE || defined(EL8)
